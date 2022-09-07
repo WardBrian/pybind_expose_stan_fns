2d29e540db772d10412da46fd7cf3914fc9b77ef
Brian Ward <bward@flatironinstitute.org>
2022-09-07 12:52:10 -0400

    Start setuptools version

---
+++ setuptools.py
@@ -0,0 +1,37 @@
+from pybind11.setup_helpers import Pybind11Extension
+import os
+from setuptools import setup
+
+
+cmdstan_path = os.environ["CMDSTAN"]
+
+include_dirs = [
+    cmdstan_path + "/stan/lib/stan_math/lib/tbb_2020.3/include",
+    cmdstan_path + "/stan/src",
+    cmdstan_path + "/lib/rapidjson_1.1.0/",
+    cmdstan_path + "/stan/lib/stan_math/",
+    cmdstan_path + "/stan/lib/stan_math/lib/eigen_3.3.9",
+    cmdstan_path + "/stan/lib/stan_math/lib/boost_1.78.0",
+    cmdstan_path + "/stan/lib/stan_math/lib/sundials_6.1.1/include",
+    cmdstan_path + "/stan/lib/stan_math/lib/sundials_6.1.1/src/sundials",
+]
+
+library_dirs = [cmdstan_path + "/stan/lib/stan_math/lib/tbb"]
+runtime_dirs = [
+    cmdstan_path + "/stan/lib/stan_math/lib/tbb",
+    cmdstan_path + "/stan/lib/stan_math/lib/sundials_6.1.1/lib/",
+]
+
+# see https://setuptools.pypa.io/en/latest/userguide/ext_modules.html#setuptools.Extension
+basic = Pybind11Extension(
+    "basic",
+    sources=["basic.cpp"],
+    include_dirs=include_dirs,
+    define_macros=[("_REENTRANT", None), ("BOOST_DISABLE_ASSERTS", None)],
+    libraries=["m", "pthread", "tbb"],
+    library_dirs=library_dirs,
+    runtime_library_dirs=runtime_dirs,
+)
+
+
+setup(ext_modules=[basic])
