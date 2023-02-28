beae208b52caa9a42a69cf7b83cb3b4950909e7f
Brian Ward <bward@flatironinstitute.org>
2023-02-28 15:43:11 -0500

    Add a working script for Windows, needs more development

---
+++ misc/build.ps1
@@ -0,0 +1,29 @@
+# ASSUMES:
+# 1. MSVC build tools and the optional Clang/LLVM extension are installed
+# 2. Sundials and TBB are installed via conda
+# 3. CmdStan or some other copy of the Stan sources is downloaded
+
+$cmdstan_location = "../cmdstan"
+
+
+Invoke-Expression "$cmdstan_location\bin\stanc.exe --standalone-functions --o=basic.cpp basic.stan"
+
+python ./pybind_stan_fns/preprocess.py basic.cpp
+
+$conda_library_path = "C:\ProgramData\Miniconda3\envs\clangtest\Library"
+$cpython_libs = "C:\ProgramData\Miniconda3\pkgs\python-3.8.15-h4de0772_0_cpython\libs"
+
+$extension = python -c "from distutils import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))"
+$pybind_includes = python -m pybind11 --includes
+
+$cxxflags = '-std=c++1y -O3 -D_REENTRANT -DBOOST_DISABLE_ASSERTS -D_BOOST_LGAMMA -DTBB_INTERFACE_NEW -Wno-sign-compare -Wno-deprecated-builtins -Wno-ignored-attributes -shared'
+$cppflags = "-I $conda_library_path\include\ -I $cmdstan_location/stan/src -I $cmdstan_location/stan/lib/rapidjson_1.1.0/ -I $cmdstan_location/stan/lib/stan_math/ -I $cmdstan_location/stan/lib/stan_math/lib/eigen_3.3.9 -I $cmdstan_location/stan/lib/stan_math/lib/boost_1.78.0 $pybind_includes"
+
+$linkflags = "-Wl`",/LIBPATH:$conda_library_path\lib\`" -Wl`",/LIBPATH:$cpython_libs`""
+$linklibs = '-ltbb -lsundials_nvecserial -lsundials_cvodes -lsundials_idas -lsundials_kinsol -lpthread'
+
+Invoke-Expression "clang++ $cppflags $cxxflags -o basic$extension basic.cpp $linkflags $linklibs"
+
+rm basic.*.exp
+rm basic.*.lib
+
