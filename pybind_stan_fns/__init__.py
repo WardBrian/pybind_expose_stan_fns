63b2181ba00cec58be8d8d71f5f50dc431cf532e
Brian Ward <bward@flatironinstitute.org>
2023-03-01 10:37:42 -0500

    Construct compiler command by pieces (#1)
    * Construct compiler command by pieces
    
    Prep for possible Windows support
    
    * Change macos specific args
    
    * Clean up

--- pybind_stan_fns/__init__.py
+++ pybind_stan_fns/__init__.py
@@ -1,28 +1,108 @@
 # will probably only work on Unix-like systems.
 
 import importlib
+import os
 import platform
 import subprocess
 import sys
+import sysconfig
+from distutils import sysconfig as dist_sysconfig
 from pathlib import Path
 
 import cmdstanpy
+import pybind11
 
 from . import preprocess
 
-stanc = Path(cmdstanpy.cmdstan_path()) / "bin" / "stanc"
+
+def get_pybind_includes():
+    # copied from pybind11.__main__
+    dirs = [
+        sysconfig.get_path("include"),
+        sysconfig.get_path("platinclude"),
+        pybind11.get_include(),
+    ]
+
+    # Make unique but preserve order
+    unique_dirs = []
+    for d in dirs:
+        if d and d not in unique_dirs:
+            unique_dirs.append(d)
+    return unique_dirs
 
 
-# TODO use make print-TBB_TARGETS and make print-LDFLAGS-TBB instead to support conda
+CMDSTAN = Path(cmdstanpy.cmdstan_path())
+stanc = CMDSTAN / "bin" / "stanc"
+
+CPP_DEFINES = ["_REENTRANT", "BOOST_DISABLE_ASSERTS"]
+
+LIBRARIES = [
+    "tbb",
+    "sundials_nvecserial",
+    "sundials_cvodes",
+    "sundials_idas",
+    "sundials_kinsol",
+    "pthread",
+]
+
+CMDSTAN_SUB_INCLUDES = [
+    ("stan", "src"),
+    ("stan", "lib", "rapidjson_1.1.0"),
+    ("stan", "lib", "stan_math"),
+    ("stan", "lib", "stan_math", "lib", "eigen_3.3.9"),
+    ("stan", "lib", "stan_math", "lib", "boost_1.78.0"),
+]
+
+OTHER_INCLUDES = []
+
+CXX_FLAGS = [
+    "-std=c++1y",
+    "-O3",
+    "-Wno-sign-compare",
+    "-Wno-deprecated-builtins",
+    "-Wno-ignored-attributes",
+    "-shared",
+]
+
+CXX = "g++"
+
+if platform.system() == "Windows":
+    CXX = "clang++.exe"
+    CPP_DEFINES.extend(["_BOOST_LGAMMA", "TBB_INTERFACE_NEW"])
+    CONDA_PATH = Path(os.environ["CONDA_PREFIX"])
+    OTHER_INCLUDES.append(str(CONDA_PATH / "Library" / "include"))
+    LDFLAGS = [
+        f'-Wl",/LIBPATH:{CONDA_PATH / "Library" / "lib"}"'
+    ]  # TODO handle python3.dll
+else:  # unix
+    CXX_FLAGS.extend(["-fPIC", "-fvisibility=hidden"])
+    LIBRARIES.append("m")
+    LDFLAGS = [
+        f'-Wl,-L,"{CMDSTAN}/stan/lib/stan_math/lib/tbb"',
+        f'-Wl,-L,"{CMDSTAN}/stan/lib/stan_math/lib/sundials_6.1.1/lib"',
+        f'-Wl,-rpath,"{CMDSTAN}/stan/lib/stan_math/lib/tbb"',
+    ]
+    CMDSTAN_SUB_INCLUDES.extend(
+        [
+            ("stan", "lib", "stan_math", "lib", "tbb_2020.3", "include"),
+            ("stan", "lib", "stan_math", "lib", "sundials_6.1.1", "include"),
+            ("stan", "lib", "stan_math", "lib", "sundials_6.1.1", "src", "sundials"),
+        ]
+    )
+
 if platform.system() == "Darwin":
-    TBB_DLL_EXT = ".dylib"
-    EXTRA_ARGS = "-undefined dynamic_lookup"
-else:  # Presumed linux
-    TBB_DLL_EXT = ".so.2"
-    EXTRA_ARGS = ""
+    CXX = "clang++"
+    CXX_FLAGS.extend(["-undefined", "dynamic_lookup"])
+
+CMDSTAN_INCLUDE_PATHS = [str(CMDSTAN.joinpath(*sub)) for sub in CMDSTAN_SUB_INCLUDES]
 
 
-# TODO use cmdstan_path rather than $CMDSTAN?
+CPP_FLAGS = [f"-D{define}" for define in CPP_DEFINES] + [
+    f"-I{path}"
+    for path in CMDSTAN_INCLUDE_PATHS + OTHER_INCLUDES + get_pybind_includes()
+]
+EXT_SUFFIX = dist_sysconfig.get_config_var("EXT_SUFFIX")
+LDLIBS = [f"-l{lib}" for lib in LIBRARIES]
 
 
 def expose(file: str):
@@ -41,11 +121,20 @@ def expose(file: str):
         str(file_path.parent / file_path.stem) + ".cpp-pre",
         out=(str(file_path.parent / file_path.stem) + ".cpp"),
     )
+    CMD = (
+        [CXX]
+        + CXX_FLAGS
+        + CPP_FLAGS
+        + [
+            f"-o{file_path.parent / file_path.stem}{EXT_SUFFIX}",
+            f"{file_path.parent / file_path.stem}.cpp",
+        ]
+        + LDFLAGS
+        + LDLIBS
+    )
+
     res = subprocess.run(
-        [
-            f'g++ {EXTRA_ARGS} -std=c++1y -D_REENTRANT -Wno-sign-compare -Wno-ignored-attributes -I "$CMDSTAN"/stan/lib/stan_math/lib/tbb_2020.3/include -O3 -I "$CMDSTAN"/stan/src -I "$CMDSTAN"/lib/rapidjson_1.1.0/ -I "$CMDSTAN"/stan/lib/stan_math/ -I "$CMDSTAN"/stan/lib/stan_math/lib/eigen_3.3.9 -I "$CMDSTAN"/stan/lib/stan_math/lib/boost_1.78.0 -I "$CMDSTAN"/stan/lib/stan_math/lib/sundials_6.1.1/include -I $CMDSTAN/stan/lib/stan_math/lib/sundials_6.1.1/src/sundials-DBOOST_DISABLE_ASSERTS'  # noqa
-            f' $(python3 -m pybind11 --includes) -shared  -fvisibility=hidden -lm -fPIC {file_path.parent / file_path.stem}.cpp -o "{file_path.parent / file_path.stem}$(python3-config --extension-suffix)" -Wl,-L,"$CMDSTAN/stan/lib/stan_math/lib/tbb" -Wl,-rpath,"$CMDSTAN/stan/lib/stan_math/lib/tbb" -lpthread -Wl,-L,"$CMDSTAN/stan/lib/stan_math/lib/tbb" -Wl,-rpath,"$CMDSTAN/stan/lib/stan_math/lib/tbb" "$CMDSTAN"/stan/lib/stan_math/lib/sundials_6.1.1/lib/libsundials_nvecserial.a "$CMDSTAN"/stan/lib/stan_math/lib/sundials_6.1.1/lib/libsundials_cvodes.a "$CMDSTAN"/stan/lib/stan_math/lib/sundials_6.1.1/lib/libsundials_idas.a "$CMDSTAN"/stan/lib/stan_math/lib/sundials_6.1.1/lib/libsundials_kinsol.a "$CMDSTAN"/stan/lib/stan_math/lib/tbb/libtbb{TBB_DLL_EXT}'  # noqa
-        ],
+        " ".join(CMD),  # TODO investigate if can use shell=False
         shell=True,
         check=False,
         capture_output=True,
